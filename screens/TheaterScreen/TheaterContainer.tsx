748b9b8c283571f0d0258b1fd66a58f5c02f9c26
1000ship <cjstjdgur1234@gmail.com>
2020-10-27 00:23:43 +0900

    fix: Fix bug that youtube-player doesn't stop when app state is inactive

--- screens/TheaterScreen/TheaterContainer.tsx
+++ screens/TheaterScreen/TheaterContainer.tsx
@@ -1,14 +1,17 @@
 import React, { useState, useCallback, useRef, useEffect } from "react";
-import { Alert, DeviceEventEmitter } from "react-native";
+import { Alert, AppState, DeviceEventEmitter } from "react-native";
 import { highlightApi } from "../../api";
 import TheaterPresenter from "./TheaterPresenter";
 
-const TheaterContainer:React.FC = ( {route:{params}} : any ) => {
-
-  const {videoId = "lotCMV_HeVg"} = params as any;
+const TheaterContainer: React.FC = ({ route: { params } }: any) => {
+  const { videoId = "lotCMV_HeVg" } = params as any;
 
   const [playing, setPlaying] = useState(false);
-  const [state, setState] = useState({highlights:[], loading: true, error: null});
+  const [state, setState] = useState({
+    highlights: [],
+    loading: true,
+    error: null,
+  });
 
   const onChangeState = useCallback((state: String) => {
     if (state === "ended") {
@@ -21,23 +24,34 @@ const TheaterContainer:React.FC = ( {route:{params}} : any ) => {
     setPlaying((prev: boolean) => !prev);
   }, []);
 
-  useEffect(function(){
-    const init = async () => {
-      setState(state => ({...state, loading: true}))
-      try {
-        const {data} = await highlightApi.getHighlights(videoId);
-        setState(state => ({...state, highlights: data.highlights || [], loading: false}));
-      }
-      catch (error) {
-        setState(state => ({...state, loading: false, error}))
-      }
+  const appStateChanged = useCallback((nextState) => {
+    if (nextState === "inactive") {
+      setPlaying(false);
     }
+  }, []);
+
+  useEffect(function () {
+    const init = async () => {
+      setState((state) => ({ ...state, loading: true }));
+      try {
+        const { data } = await highlightApi.getHighlights(videoId);
+        setState((state) => ({
+          ...state,
+          highlights: data.highlights || [],
+          loading: false,
+        }));
+      } catch (error) {
+        setState((state) => ({ ...state, loading: false, error }));
+      }
+    };
     init();
+    AppState.addEventListener("change", appStateChanged);
 
     return () => {
+      AppState.removeEventListener("change", appStateChanged);
       DeviceEventEmitter.emit("popTheater");
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     <TheaterPresenter
