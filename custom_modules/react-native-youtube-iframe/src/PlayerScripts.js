0454df01e25e2cf76fdec398b95a88f879b4278c
1000ship <cjstjdgur1234@gmail.com>
2020-10-28 19:17:26 +0900

    chore: Change 'react-native-youtube-iframe' module to custom

---
+++ custom_modules/react-native-youtube-iframe/src/PlayerScripts.js
@@ -0,0 +1,189 @@
+import {MUTE_MODE, PAUSE_MODE, PLAY_MODE, UNMUTE_MODE} from './constants';
+
+export const PLAYER_FUNCTIONS = {
+  muteVideo: 'player.mute(); true;',
+  unMuteVideo: 'player.unMute(); true;',
+  playVideo: 'player.playVideo(); true;',
+  pauseVideo: 'player.pauseVideo(); true;',
+  durationScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getDuration', data: player.getDuration()}));
+true;
+`,
+  currentTimeScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getCurrentTime', data: player.getCurrentTime()}));
+true;
+`,
+  isMutedScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'isMuted', data: player.isMuted()}));
+true;
+`,
+  getVolumeScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getVolume', data: player.getVolume()}));
+true;
+`,
+  getPlaybackRateScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getPlaybackRate', data: player.getPlaybackRate()}));
+true;
+`,
+  getAvailablePlaybackRatesScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getAvailablePlaybackRates', data: player.getAvailablePlaybackRates()}));
+true;
+`,
+
+  setVolume: volume => {
+    return `player.setVolume(${volume}); true;`;
+  },
+
+  seekToScript: (seconds, allowSeekAhead) => {
+    return `player.seekTo(${seconds}, ${allowSeekAhead}); true;`;
+  },
+
+  setPlaybackRate: playbackRate => {
+    return `player.setPlaybackRate(${playbackRate}); true;`;
+  },
+
+  loadPlaylist: (playList, startIndex, play) => {
+    const index = startIndex || 0;
+    const playlistJson = JSON.stringify(playList);
+    const func = play ? 'loadPlaylist' : 'cuePlaylist';
+
+    return `player.${func}({playlist: ${playlistJson}, index: ${index}); true;`;
+  },
+};
+
+export const playMode = {
+  [PLAY_MODE]: PLAYER_FUNCTIONS.playVideo,
+  [PAUSE_MODE]: PLAYER_FUNCTIONS.pauseVideo,
+};
+
+export const soundMode = {
+  [MUTE_MODE]: PLAYER_FUNCTIONS.muteVideo,
+  [UNMUTE_MODE]: PLAYER_FUNCTIONS.unMuteVideo,
+};
+
+export const MAIN_SCRIPT = (
+  videoId,
+  playList,
+  {
+    loop = false,
+    controls = true,
+    cc_lang_pref, // country code
+    showClosedCaptions,
+    color, // 'red' or 'white'
+    end,
+    preventFullScreen = false,
+    playerLang,
+    iv_load_policy,
+    modestbranding,
+    rel,
+    start,
+  },
+  allowWebViewZoom,
+  uiScale
+) => `<!DOCTYPE html>
+<html>
+  <head>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=${uiScale}${
+        allowWebViewZoom ? '' : `, maximum-scale=${uiScale}`
+      }"
+    >
+    <style>
+      body {
+        margin: 0;
+      }
+      .container {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-bottom: 56.25%;
+      }
+      .video {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="video" id="player" />
+    </div>
+
+    <script>
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '1000',
+          width: '1000',
+          videoId: '${videoId || ''}',
+          playerVars: {
+            playsinline: 1,
+            loop: ${loop ? 1 : 0},
+            controls: ${controls ? 1 : 0},
+            cc_lang_pref: '${cc_lang_pref || ''}',
+            cc_load_policy: ${showClosedCaptions ? 1 : 0},
+            color: ${color},
+            end: ${end},
+            fs: ${preventFullScreen ? 0 : 1},
+            hl: ${playerLang},
+            iv_load_policy: ${iv_load_policy},
+            modestbranding: ${modestbranding ? 1 : 0},
+            rel: ${rel ? 1 : 0},
+            start: ${start},
+            listType:  '${typeof playList === 'string' ? 'playlist' : ''}',
+            list: '${typeof playList === 'string' ? playList : ''}',
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange,
+            'onError': onPlayerError,
+            'onPlaybackQualityChange': onPlaybackQualityChange,
+            'onPlaybackRateChange': onPlaybackRateChange,
+          }
+        });
+      }
+
+      function onPlayerError(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerError', data: event.data}))
+      }
+
+      function onPlaybackRateChange(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playbackRateChange', data: event.data}))
+      }
+
+      function onPlaybackQualityChange(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerQualityChange', data: event.data}))
+      }
+
+      function onPlayerReady(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerReady'}))
+      }
+
+      var done = false;
+      function onPlayerStateChange(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerStateChange', data: event.data}))
+      }
+
+      var isFullScreen = false;
+      function onFullScreenChange() {
+        isFullScreen = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'fullScreenChange', data: Boolean(isFullScreen)}));
+      }
+
+      document.addEventListener('fullscreenchange', onFullScreenChange)
+      document.addEventListener('mozfullscreenchange', onFullScreenChange)
+      document.addEventListener('msfullscreenchange', onFullScreenChange)
+      document.addEventListener('webkitfullscreenchange', onFullScreenChange)
+    </script>
+  </body>
+</html>`;
